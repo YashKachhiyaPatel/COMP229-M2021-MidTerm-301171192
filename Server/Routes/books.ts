bb624ced159f67a13363da6ae4966fecd634705d
YashKachhiyaPatel <lifeline242734@gmail.com>
2021-06-22 15:36:41 -0700

    delete functions added

--- Server/Routes/books.ts
+++ Server/Routes/books.ts
@@ -99,9 +99,17 @@ router.post('/:id', (req, res, next) => {
 // GET - process the delete by user id
 router.get('/delete/:id', (req, res, next) => {
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+  let id = req.params.id;
+
+  // db.clothing.remove({"_id: id"})
+  book.remove({ _id: id }, (err) => {
+    if (err) {
+      console.error(err);
+      res.end(err);
+    }
+
+    res.redirect('/books');
+  });
 });
 
 
